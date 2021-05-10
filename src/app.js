fba1710150e98a4a0938c5a5821324c9d0f201ca
highway92 <highway92@kakao.com>
2021-05-10 13:07:10 +0900

    prepareing deploy

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
+import "@babel/polyfill";
 import express from "express";
 import routes from "./routes";
+import path from "path";
 import cookieParser from "cookie-parser";
 import globalRouter from "./globalRouter";
 import { localsMiddleware } from "./middlewares";
@@ -7,8 +9,8 @@ import { localsMiddleware } from "./middlewares";
 const app = express();
 
 app.set("view engine", "pug");
-app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("static"));
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 app.use(express.static("public"));
 
 app.use(cookieParser());
